5c771edaa06e56ee42cf3a555ccada2ecfefe274
l08084 <l08084.1989@gmail.com>
2019-06-26 18:17:04 +0900

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  post: {
+    userName: string;
+    message: string;
+    createdDate: any;
+  } = {
+    userName: 'Taro Yamada',
+    message: 'これはテキストメッセージです',
+    createdDate: '10分前'
+  };
 
   constructor() {}
-
 }
