fb24d729620c5211f7a54846cc7d81883fb9f0b1
l08084 <l08084.1989@gmail.com>
2019-07-14 20:04:38 +0900

    add component class

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+// Firebase
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
+  login: {
+    email: string;
+    password: string;
+  } = {
+    email: '',
+    password: ''
+  };
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private toastCtrl: ToastController,
+    private afAuth: AngularFireAuth
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  userLogin() {
+    this.afAuth.auth
+      .signInWithEmailAndPassword(this.login.email, this.login.password)
+      .then(async user => {
+        const toast = await this.toastCtrl.create({
+          message: 'ログインできました',
+          duration: 3000
+        });
+        await toast.present();
+        // ログインできたらメッセージボードに移動する
+        this.router.navigate(['/home']);
+      })
+      .catch(async error => {
+        const toast = await this.toastCtrl.create({
+          message: error.toString(),
+          duration: 3000
+        });
+        await toast.present();
+      });
   }
-
 }
