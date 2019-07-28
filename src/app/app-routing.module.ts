c8f7d824476f2446d72d3c1bfb100d801d76de7c
l08084 <l08084.1989@gmail.com>
2019-07-28 17:11:03 +0900

    add login guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginGuard } from './login.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomePageModule',
+    canActivate: [LoginGuard]
+  },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' }
 ];
