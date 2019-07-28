191a7c929f777c254de3b33727a843464eb8c435
l08084 <l08084.1989@gmail.com>
2019-07-28 19:13:04 +0900

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginGuard } from './login.guard';
+import { LoggedinGuard } from './loggedin.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -9,8 +10,16 @@ const routes: Routes = [
     loadChildren: './home/home.module#HomePageModule',
     canActivate: [LoginGuard]
   },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' }
+  {
+    path: 'login',
+    loadChildren: './login/login.module#LoginPageModule',
+    canActivate: [LoggedinGuard]
+  },
+  {
+    path: 'signup',
+    loadChildren: './signup/signup.module#SignupPageModule',
+    canActivate: [LoggedinGuard]
+  }
 ];
 
 @NgModule({
